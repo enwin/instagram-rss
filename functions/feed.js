70af7ea95d008f022f762260a97636473e258dc3
Thomas Beduneau <thomas@switch-company.com>
2019-12-18 11:50:57 +0100

    first version

---
+++ functions/feed.js
@@ -0,0 +1,79 @@
+const https = require('https');
+const ParserStream = require('parse5-parser-stream');
+const RSS = require( 'rss' );
+
+exports.handler = function(event, context, callback) {
+  return new Promise((resolve, reject) => {
+    https.get('https://www.instagram.com/cloudwaterbrew/', res => {
+        const parser = new ParserStream();
+
+        parser.on('script', (scriptElement, documentWrite, resume) => {
+          let content = scriptElement.childNodes.find(({nodeName}) => nodeName === '#text');
+
+          if( !content || (content && !content.value.startsWith( 'window._sharedData' )) ){
+            return resume();
+          }
+
+          const value = content.value.replace('window._sharedData = ', '').slice(0, -1);
+
+          try{
+            resolve(JSON.parse(value));
+            resume();
+          }
+          catch(e){
+            reject( e )
+          }
+
+        });
+
+        const chunks = [];
+
+        res.on("data", (chunk) => {
+          chunks.push(chunk);
+        });
+
+        res.on("error", (error)  =>{
+          reject(error);
+        });
+
+        res.on("end", ()  =>{
+          parser.end( Buffer.concat(chunks).toString());
+        });
+      });
+  })
+  .then( data => {
+    const user = data.entry_data.ProfilePage[0].graphql.user;
+
+    const feed = new RSS({
+      title: user.full_name,
+      description: user.biography,
+      feed_url: process.env.URL,
+      pubDate: new Date(),
+    });
+
+    user.edge_owner_to_timeline_media.edges.forEach( ({node}) => {
+      feed.item({
+        title: node.accessibility_caption,
+        date: new Date( node.taken_at_timestamp * 1000 ),
+        enclosure:{
+          url  : node.display_url,
+          type : 'image/jpeg'
+        }
+      });
+    })
+
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/xml; charset=utf-8'
+      },
+      body: feed.xml({indent: true})
+    }
+  })
+  .catch(error => {
+    return {
+      statusCode: 500,
+      body: error.message
+    }
+  })
+}
